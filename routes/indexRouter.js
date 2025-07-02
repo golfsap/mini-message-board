3e584990963c0ba1cc814c6bfb547bac25aa61f4
Golfsap <golf.sap@gmail.com>
2025-07-02 15:24:33 +0700

    Add open button to view message details page

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,11 +3,13 @@ const indexRouter = Router();
 
 const messages = [
   {
+    id: 1,
     text: "Hi there!",
     user: "Amando",
     added: new Date(),
   },
   {
+    id: 2,
     text: "Hello World",
     user: "Charles",
     added: new Date(),
@@ -22,10 +24,28 @@ indexRouter.get("/new", (req, res) => {
   res.render("form");
 });
 
+indexRouter.get("/messages/:id", (req, res) => {
+  const messageId = parseInt(req.params.id);
+  const message = messages.find((message) => message.id === messageId);
+
+  if (!message) {
+    return res.status(404).send("Message not found");
+  }
+
+  res.render("message", { title: "Message Details", message });
+});
+
 indexRouter.post("/new", (req, res) => {
   const { messageUser, messageText } = req.body;
   if (messageUser && messageText) {
-    messages.push({ text: messageText, user: messageUser, added: new Date() });
+    const newId =
+      messages.length > 0 ? messages[messages.length - 1].id + 1 : 1;
+    messages.push({
+      id: newId,
+      text: messageText,
+      user: messageUser,
+      added: new Date(),
+    });
   }
   res.redirect("/");
 });
